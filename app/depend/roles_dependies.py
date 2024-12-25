3895e4f2c0828821a7def9ba28943c7bf012f079
dima cheban <dimacheban23@gmail.com>
2024-12-25 14:10:18 +0200

    refactor: convert role_dependency to class-based implementation

--- app/depend/roles_dependies.py
+++ app/depend/roles_dependies.py
@@ -4,35 +4,44 @@ from app.config.pack_roles import Role
 from app.services.base import service
 from app.db.get_session import get_connection_db
 from functools import partial
+from app.models.base_model import Users
+
+class RoleDepandency:
+    def __init__(self, allowed_roles: list[Role]):
+        """
+        зависимость для проверки ролей
+        Args:
+            list[Role] - передаваемый список допустимых ролей
+        """
+        self.allowed_roles = allowed_roles
+    
+    async def __call__(
+            self,
+            token:str = Depends(service.auth.auth2_scheme),
+            db: AsyncSession = Depends(get_connection_db)
+    )->Users:
+        """
+        проверка  роли пользователя
+        
+        Args:
+            token(str): токен авторизации
+            db: (AsyncSession): ассинхронная сессия базы данных
+        
+        Resturns:
+            Users: обьект пользователя
+        
+        Raises:
+            HTTPExceptions: если пользовател не имеет разрещенной роли
+        """
+        user = await service.auth.check_roles(token, db, self.allowed_roles)
+        return user
 
 
 
-async def role_dependency(
-        allowed_roles = list[Role],
-        token:str = Depends(service.auth.auth2_scheme),
-        db:AsyncSession = Depends(get_connection_db)
-):
-    """зависимосмость для проверки ролей
-    Args:
-        list[Role] - передаваемый список допустимых ролей
-        token: зависимось которая проверет access токен
-        db: зависимось которая предоставляет выдачу асинхронной сессия бд
+role_dependency_admin = RoleDepandency(allowed_roles=[Role.ADMIN])
 
-    role_dependency:
-        token
-        db
-        allowed roles: розрещшенный пользователи
-
-        Description:
-            получает обьект пользователя с базы данных
-            если роль пользователя есть в переданном списке
-            возвращает пользователя
-    Reautrns:
-        usres
-    """
-    return await service.auth.check_roles(token, db, allowed_roles)
-
-role_dependency_admin = partial(role_dependency, allowed_roles=[Role.ADMIN,])
-role_dependency_all = partial(role_dependency, allowed_roles = [
-    Role.ADMIN, Role.MODERATOR, Role.USER
-])
+role_dependency_all = RoleDepandency(allowed_roles = [
+    Role.ADMIN, 
+    Role.MODERATOR, 
+    Role.USER
+    ])
